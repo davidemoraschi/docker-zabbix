508b6dce31cc6789200d26bf2b53f3aa1f9733f3
Davide Moraschi <davide.moraschi@toptal.com>
2024-01-08 17:57:50 +0100

    Update get_jobs.cmd

--- get_jobs.cmd
+++ get_jobs.cmd
@@ -3,3 +3,9 @@ docker run --rm -ti -v %userprofile%\.aws:/root/.aws -v %cd%:/aws amazon/aws-cli
 docker run --rm -ti -v %userprofile%\.aws:/root/.aws -v %cd%:/aws amazon/aws-cli glue get-job-runs --job-name Sample_job_01 --output json --query "JobRuns[*].[Id,JobRunState,StartedOn]"
 
 docker exec -it -u 0 zabbix-agent aws glue get-job-runs --job-name Sample_job_01
+
+aws glue get-job-runs --job-name Sample_job_01 --max-items 1 --output json --query 'JobRuns[0].JobRunState'
+out=$(aws glue get-job-runs --job-name Sample_job_01 --max-items 1 --output json --query "JobRuns[0].JobRunState")
+zabbix_sender -z zabbix-server -p 10051 -s "zabbix-server" -k Sample_job_01 -o "$out"
+
+
